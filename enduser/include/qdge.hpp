ebd242b2e0cb628e13180baa3b331ea14feb0500
W1nd0w55 <iliader@outlook.com>
2025-06-23 21:57:19 +0500

    Added skecthy core log macro undefines

--- enduser/include/qdge.hpp
+++ enduser/include/qdge.hpp
@@ -4,3 +4,10 @@
 #include "Exceptions.hpp"
 #include "Tools/Log.hpp"
 #include "Helpers/EntryPoint.hpp"
+
+#undef QDGE_TRACE
+#undef QDGE_DEBUG
+#undef QDGE_INFO
+#undef QDGE_WARN
+#undef QDGE_ERROR
+#undef QDGE_CRITICAL
